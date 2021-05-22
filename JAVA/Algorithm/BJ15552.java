526f4b3cc4d38de6e826d6f94b01912b63320fd9
jinsunkim <jinsunkim@gmail.com>
2021-05-22 22:07:19 +0900

    start BJ15552

---
+++ JAVA/Algorithm/BJ15552.java
@@ -0,0 +1,7 @@
+import java.io.*;
+
+public class BJ15552 {
+    public static void main(String args[]) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }       
+}
