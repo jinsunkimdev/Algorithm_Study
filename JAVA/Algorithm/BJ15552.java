3dfb558e10317dfd8f5b0dfac7a3c5dd2e5540bd
jinsunkim <jinsunkim@gmail.com>
2021-05-23 18:10:53 +0900

    210523

--- JAVA/Algorithm/BJ15552.java
+++ JAVA/Algorithm/BJ15552.java
@@ -1,7 +1,15 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
-public class BJ15552 {
-    public static void main(String args[]) {
+
+public class BJ15552{
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int i = Integer.parseInt(br.readLine());
+        StringTokenizer st; //String arr[] = str.split(" ");
+        for(int j=0;j< i;j++){
+
+        }
     }       
 }
