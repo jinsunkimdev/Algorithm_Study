e408cb4a1c754160da1339f1729d6d2c77e06296
jinsunkim <jinsunkim94@gmail.com>
2021-07-25 23:56:11 +0900

    Fix 1 BOJ problem

--- BOJ/JS/LOOP/11022.js
+++ BOJ/JS/LOOP/11022.js
@@ -3,19 +3,22 @@ const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
-const inputArr = [];
-const numbers = [];
-let count = 0;
+let inputArr = [];
+let count;
+let numbers = [];
 rl.on('line', (line) => {
 	inputArr.push(line);
 }).on('close', () => {
-	console.log(inputArr);
-	for(let i=1; i<=Number(inputArr[0]); i++){
-		console.log(inputArr);
-		numbers = inputArr[i].split(' ');
-		let str1 = numbers[0];
-		let str2 = numbers[1];
-		console.log(`Case #${i}: ${str1} + ${str2} = ${str1+str2}`);
-	};
+	const count = parseInt(inputArr[0]);
+	for(let i=1; i<=count; i++){
+		let numbers = inputArr[i].split(' ').map((el) => {
+			parseInt(el);
+		});
+		console.log(`numbers=${numbers}`);
+		let num1 = numbers[0];
+		let num2 = numbers[1];
+		console.log(`num1=${num1}, num2=${num2}`);
+		console.log(`inputArr=${inputArr}`);
+	}
 	process.exit();
 });
