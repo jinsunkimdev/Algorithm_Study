6a95dc82761d85af166af43d9ab83358bcda8af9
jinsunkim <jinsunkim94@gmail.com>
2021-08-09 23:52:24 +0900

    Solved BOJ 1 GM problem

--- BOJ/JS/_GM/1193.js
+++ BOJ/JS/_GM/1193.js
@@ -9,6 +9,19 @@ let inputNum = 0;
 rl.on('line', (line) => {
 	inputNum = Number(line);
 }).on('close', () => {
-
+	let value = 1;
+while (true) {
+	number -= value;
+	if (number <= 0) {
+		number += value;
+		break;
+	}
+	value++;
+}
+if (value % 2 === 1) {
+	console.log(`${value - (number - 1)}/${1 + (number - 1)}`);
+} else {
+	console.log(`${1 + (number - 1)}/${value - (number - 1)}`);
+}
 	process.exit();
 });
