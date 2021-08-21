5765abe3c82dd8f6fc036beb11699495d0787974
jinsunkim <jinsunkim94@gmail.com>
2021-08-21 23:52:28 +0900

    Solved BOJ GM1 part 1 problems

---
+++ BOJ/JS/_GM1/2775_3.js
@@ -0,0 +1,46 @@
+// 부녀회장이 될테야
+'use strict'
+const readline = require('readline');
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+let inputArr = [];
+//k-층,n-호
+//1~k층 (k층 n호에는 k-1층 n호+k층 n-1호만큼 산다.)
+rl.on('line', (line) => {
+	inputArr.push(line);
+}).on('close', () => {
+	let inputArrNum = inputArr.map((el) => parseInt(el));
+	let k = [];
+	let n = [];
+	let number = inputArrNum.shift();
+	for(let i=0; i<number; i++){
+		k.push(inputArrNum.shift());
+		n.push(inputArrNum.shift());
+		}
+		let knArray = [];
+		knArray.push(k, n);
+	let floor = [];
+	let sum = 0;
+	for(let i=0; i<knArray.length; i++){
+		k = knArray[i][0];
+		n = knArray[i][1];
+		for(let j=0; j<=k; j++){
+			floor[j] = [];
+			for(let m=1; m<=n; m++){
+				if(j==0){
+					floor[j].push(m);
+					}else{
+					sum += floor[j-1][m-1];
+					floor[j].push(sum);
+					if(m === n){
+						sum = 0;
+						}
+					}
+				}
+			}
+			console.log(floor[k][n-1])
+		}
+process.exit();
+});
--- BOJ/JS/_GM1/2775_err_2.js
+++ BOJ/JS/_GM1/2775_err_2.js
@@ -19,16 +19,28 @@ rl.on('line', (line) => {
 			k.push(inputNum.shift());
 			n.push(inputNum.shift());
 	}
-	console.log(k);
-	console.log(n);
-	// 각 방당 인원수를 저장할 2차원 배열 선언 
+	//각 방당 인원수를 저장할 2차원 배열
 	const house = Array.from(Array(k.length+1), () => Array(n.length+1).fill(0));
-	// 0층의 i호에는 i명이 산다
-	for(let i=1; i<=n; i++){
+	//0층에 사는 인원 구하기
+	for(let i=0; i<n.length; i++){
 		house[0][i] = i;
+		};
+	//1~k층(k층 n호에는 k-1층 n호 + k층 n-1호만큼)
+	for(let i=0; i<k.length; i++){
+		for(let j=0; j<n.length; j++){
+			house[i][j] = house[i-1][j] + house[i][j-1];
+		}
 	};
-	console.log(house);
+	//정답 출력
+	for(let i=0; i<testCount; i++){
+	console.log(house[k[i]][n[i]]);
+	};		
 	process.exit();
 });
+
+
+
+
+
 
 
