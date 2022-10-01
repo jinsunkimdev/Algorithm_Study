1ed92a65ca2e81bc9acda835eb81e964747bb5a3
jinsunkim <jinsunkim94@gmail.com>
2022-10-01 23:38:25 +0900

    221001 Sorting Problems

--- INFLEARN/SECTION7/allocConfRooms.js
+++ INFLEARN/SECTION7/allocConfRooms.js
@@ -16,7 +16,7 @@ require('readline').createInterface(process.stdin, process.stdout)
 	input.push(line.trim())
 })
 .on('close', () => {
-	const n = Number( input.shift() )
+	const N = Number( input.shift() )
 	let start = []
 	let end = []
 	for(let x of input)
@@ -25,10 +25,29 @@ require('readline').createInterface(process.stdin, process.stdout)
 		start.push( tmp[0] )
 		end.push( tmp[1] )
 	}
+	start.sort((a, b) => a - b)
+	end.sort((a, b) => a - b)
 
-	const solution = (s, e) => {
-		let answer
-		return answer
+	const solution = (s, e, n) => {
+		let answer = []
+		for(let i = 0; i < n; i++)
+		{
+			let cnt = 1
+			let idx = i
+			let j = i + 1 
+			while(j < n)
+			{
+				if(e[idx] <= s[j])
+				{
+					cnt++
+					idx = j
+					j = j + 1	
+				}
+				else j = j + 1
+			}
+				answer.push(cnt)
+		}
+		return Math.max(...answer)
 	}
-	console.log(solution(start, end))
+	console.log(solution(start, end, N))
 })
